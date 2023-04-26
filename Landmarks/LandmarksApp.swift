e4dbee50413e5010bc2f4c35afc0c1ea49c97766
Felipe Christian Lautenschlager <felipelauten@gmail.com>
2023-04-26 21:40:09 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Felipe Christian Lautenschlager on 26/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
